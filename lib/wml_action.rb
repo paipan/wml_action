eae48f60e1e6507c9a9f1051493fb41e4c6a21d3
Pancho <paul.schko@gmail.com>
2014-01-27 20:42:10 +0800

    Working commit

--- lib/wml_action.rb
+++ lib/wml_action.rb
@@ -1,6 +1,7 @@
 require "wml_action/version"
 require "wml_action/section"
 require "wml_action/action_section"
+require "wml_action/cli"
 
 module WmlAction
 
--- lib/wml_action/cli.rb
+++ lib/wml_action/cli.rb
@@ -5,9 +5,37 @@ module WmlAction
 
     class CLI < Thor
 
-        desc "wml_modifier SRC DEST", "Modifies a wml"
-        def wml_modifier(src,dst)
-            puts "wml_modifier #{src},#{dst}"
+        desc "modify SRC DEST", "Modifies a wml"
+        def modify(original,modlist)
+
+            $LOG=Logger.new(STDERR)
+            $LOG.sev_threshold = Logger::INFO
+
+            target_name=original
+            modlist_name=modlist
+
+            if not File.exist?(target_name) then
+                    $LOG.fatal "Invalid target file: #{target_name}"
+                    exit
+            end
+            if not File.exist?(modlist_name) then
+                    $LOG.fatal "Invalid modlist file: #{modlist_name}"
+                    exit
+            end
+
+            target=File.open(target_name)
+            modlist=File.open(modlist_name)
+            target_root=Section.new.fromFile(target)
+            #print target_root.dumpSection
+            
+            modlist_root=ActionSection.new.fromFile(modlist)
+            #modlist_root.dumpSection
+            
+            modlist_root.applyActionSection(target_root)
+            print target_root.dumpSection
+            
+            target.close
+            modlist.close
         end
 
     end
