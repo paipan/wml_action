9e7f79c990778eee8f848042f41a6eb9c862b94a
Pancho <paul.schko@gmail.com>
2014-06-08 23:21:09 +0800

    Deleted old section files

--- lib/wml_action.rb
+++ lib/wml_action.rb
@@ -1,11 +1,8 @@
 require "wml_action/version"
-require "wml_action/section"
 require "wml_action/action_section"
 require "wml_action/cli"
-require "wml_action/document"
 require "wml_action/action_document"
 require "action_parser.tab"
-require "parser.tab"
 require 'wml_action/log'
 
 module WmlAction
