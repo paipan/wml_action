bab6b1fcc7ed5cb07ae4942a37b04afcc978a971
Pancho <paul.schko@gmail.com>
2014-06-30 12:16:15 +0800

    CLI now accepts many modification files

--- lib/wml_action/cli.rb
+++ lib/wml_action/cli.rb
@@ -3,53 +3,58 @@ require 'wml_action'
 
 module WMLAction
 
-    class CLI < Thor
-      include Log
+  class CLI < Thor
+    include Log
 
-      class_option :verbose, type: :boolean, aliases: '-v'
-      class_option :debug, type: :boolean, aliases: '-d'
+    class_option :verbose, type: :boolean, aliases: '-v'
+    class_option :debug, type: :boolean, aliases: '-d'
 
-        desc "modify FILE MODS_FILE", "Modifies a WML"
-        def modify(original,modlist)
-            log.level=Logger::INFO if options[:verbose]
-            log.level=Logger::DEBUG if options[:debug]
+    desc "modify FILE MODS_FILES", "Modifies a WML"
+    def modify(target_name,*modlist_names)
+      log.level=Logger::INFO if options[:verbose]
+      log.level=Logger::DEBUG if options[:debug]
 
-            target_name=original
-            modlist_name=modlist
+      unless File.exist?(target_name)
+        log.fatal "Invalid target file: #{target_name}"
+        exit
+      end
 
-            unless File.exist?(target_name)
-                    log.fatal "Invalid target file: #{target_name}"
-                    exit
-            end
-            unless File.exist?(modlist_name)
-                    log.fatal "Invalid modlist file: #{modlist_name}"
-                    exit
-            end
+      if modlist_names.empty?
+        log.fatal "Need at least one modlist file"
+        exit
+      end
 
-            target=Document.from_file(target_name)
-            modlist=Document.from_file(modlist_name)
+      non_exist = modlist_names.clone.delete_if {|f| File.exist? f} 
+      unless non_exist.empty?
+        log.fatal "Modlist files does not exist: #{non_exist.join(" ")}"
+        exit
+      end
 
-            target.root.merge(modlist.root)
-            print target.root.to_s
+      target=Document.from_file(target_name)
+      modlist_names.each do |name|
+        modlist=Document.from_file(name)
+        target.root.merge(modlist.root)
+      end
 
-        end
-
-        desc "read FILE", "Reads and outputs a wml"
-        def read(filename)
-            log.level=Logger::INFO if options[:verbose]
-            log.level=Logger::DEBUG if options[:debug]
-            d=Document.from_file(filename)
-            print d.root.to_s
-        end
-
-        desc "action_read FILE", "Reads and outputs an action wml"
-        def action_read(filename)
-            log.level=Logger::INFO if options[:verbose]
-            log.level=Logger::DEBUG if options[:debug]
-            d=Document.from_file(filename)
-            print d.root.to_s
-        end
-
+      print target.root.to_s
     end
+
+    desc "read FILE", "Reads and outputs a wml"
+    def read(filename)
+      log.level=Logger::INFO if options[:verbose]
+      log.level=Logger::DEBUG if options[:debug]
+      d=Document.from_file(filename)
+      print d.root.to_s
+    end
+
+    desc "action_read FILE", "Reads and outputs an action wml"
+    def action_read(filename)
+      log.level=Logger::INFO if options[:verbose]
+      log.level=Logger::DEBUG if options[:debug]
+      d=Document.from_file(filename)
+      print d.root.to_s
+    end
+
+  end
 
 end
